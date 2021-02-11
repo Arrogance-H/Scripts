ce546c37a528ccaa96f0f37f7fb7496b6cd49617
Ronin <68095403+Arrogance-H@users.noreply.github.com>
2021-02-11 15:31:05 +0800

    Update sendNotify.js

--- sendNotify.js
+++ sendNotify.js
@@ -8,7 +8,7 @@ let SCKEY = '';
 // =======================================Bark App通知设置区域===========================================
 //此处填你BarkAPP的信息(IP/设备码，例如：https://api.day.app/XXXXXXXX)
 //注：此处设置github action用户填写到Settings-Secrets里面（Name输入BARK_PUSH）
-let BARK_PUSH = '';
+let BARK_PUSH = 'c5jw7jPB9XDxhLPYbtzTgG';
 //BARK app推送铃声,铃声列表去APP查看复制填写
 //注：此处设置github action用户填写到Settings-Secrets里面（Name输入BARK_SOUND , Value输入app提供的铃声名称，例如:birdsong）
 let BARK_SOUND = '';
