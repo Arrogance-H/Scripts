83382e280d22a20eed62b085a286b54cbffc7745
Ronin <68095403+Arrogance-H@users.noreply.github.com>
2021-02-15 15:47:01 +0800

    Update ranlv.js

--- ranlv.js
+++ ranlv.js
@@ -74,6 +74,16 @@ if ($.isNode()) {
   } else {
    rlheader = process.env.RLHEADER.split()
   };
+ Object.keys(rlurl).forEach((item) => {
+        if (rlurl[item]) {
+          rlurlArr.push(rlurl[item])
+        }
+    });
+    Object.keys(rlheader).forEach((item) => {
+        if (rlheader[item]) {
+          rlheaderArr.push(rlheader[item])
+        }
+    });
 
     console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString()}  =============\n`)
     console.log(`============ 脚本执行-北京时间(UTC+8)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}  =============\n`)
