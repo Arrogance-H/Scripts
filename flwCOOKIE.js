85fef52673dd4c6806ef0907dc226b1696af2352
Ronin <68095403+Arrogance-H@users.noreply.github.com>
2021-02-16 13:21:47 +0800

    Create flwCOOKIE.js

---
+++ flwCOOKIE.js
@@ -0,0 +1,22 @@
+
+
+//独立COOKIE文件     ck在``里面填写，多账号换行
+let flwurlVal= ``
+let flwheaderVal= ``
+let flwspbodyVal= ``
+let flwqwbodyVal= ``
+
+
+
+let flwcookie = {
+  flwurlVal: flwurlVal,  
+  flwheaderVal: flwheaderVal,  
+  flwspbodyVal: flwspbodyVal, 
+  flwqwbodyVal: flwqwbodyVal, 
+
+
+}
+
+module.exports =  flwcookie
+  
+
