5527c11d2555793dca4c62a1a2fe9ea5aa59aa2f
Ronin <68095403+Arrogance-H@users.noreply.github.com>
2021-02-19 13:10:27 +0800

    Update dkd.js

--- dkd.js
+++ dkd.js
@@ -76,7 +76,7 @@ duokandianheaderVal = {
 };
 if ($.isNode()) {
     // 没有设置 DKD_duokandianCASH 则默认为 0 不提现
-    CASH = process.env.DKD_duokandianCASH || 15;
+    CASH = process.env.DKD_duokandianCASH || 1;
 }
 if ($.isNode() && process.env.DKD_duokandianBODY) {
     COOKIES_SPLIT = process.env.COOKIES_SPLIT || "\n";
