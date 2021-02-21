3edb56d423020f4019c5a8c11bc4c859fafc921d
Ronin <68095403+Arrogance-H@users.noreply.github.com>
2021-02-21 14:39:26 +0800

    Update fqkk.js

--- fqkk.js
+++ fqkk.js
@@ -222,11 +222,11 @@ let url = {
 //番茄看看key
 function fqkk1(timeout = 0) {
   return new Promise((resolve) => {
-    setTimeout( ()=>{
+    /*setTimeout( ()=>{
       if (typeof $.getdata('fqkkhd') === "undefined") {
         $.msg($.name,"",'请先获取番茄看看数据!😓',)
         $.done()
-      }
+      }*/
 let url = {
         url : "http://m."+fqkkurl.match(/m.(.*?).top/)[1]+".top/reada/getTask",
         headers : JSON.parse(fqkkhd),
