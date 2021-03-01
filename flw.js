41c7cf45818f174f6e5e54aaaa2527bcec8eb731
Ronin <68095403+Arrogance-H@users.noreply.github.com>
2021-03-01 12:37:56 +0800

    Update flw.js

--- flw.js
+++ flw.js
@@ -103,7 +103,7 @@ let middleflwqwBODY = [];
 let middleflwydBODY = [];
 // 没有设置 FL_CASH 则默认为 0 不提现
 if ($.isNode()) {
-  CASH = process.env.FL_CASH || 0;
+  CASH = process.env.FL_CASH || 1;
   // 没有设置 FL_DHCASH 则默认为 0 不兑换
   DHCASH = process.env.FL_DHCASH || 5;
 }
