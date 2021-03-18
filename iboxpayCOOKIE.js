04592f15056358f642cf53c945601030e3fc6559
Ronin <68095403+Arrogance-H@users.noreply.github.com>
2021-03-18 11:17:27 +0800

    Update iboxpayCOOKIE.js

--- iboxpayCOOKIE.js
+++ iboxpayCOOKIE.js
@@ -3,11 +3,15 @@
 //独立COOKIE文件     ck在``里面填写，多账号换行
 
 let refreshtokenVal= ``
+let iboxpayvideoheaderVal= ``
+let iboxpayvideobodyVal= ``
 
 
 let iboxpaycookie = {
 
-  refreshtokenVal: refreshtokenVal,  
+  refreshtokenVal: refreshtokenVal, 
+    iboxpayvideoheaderVal: iboxpayvideoheaderVal,  
+      iboxpayvideobodyVal: iboxpayvideobodyVal,   
   
 }
 
